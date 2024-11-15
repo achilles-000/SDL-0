82690d8509f69af4a6f9f6865ba302197bcbf2ee
achilles-000 <stefanpopovic976@gmail.com>
2024-11-15 10:40:36 -0500

    SDL with claude code

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window *window = SDL_CreateWindow("Movable Window",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (window == NULL) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Main loop
+    SDL_bool running = SDL_TRUE;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = SDL_FALSE;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        // Save the initial window position
+                        int initialX, initialY;
+                        SDL_GetWindowPosition(window, &initialX, &initialY);
+
+                        // Move the window with the mouse
+                        int windowX, windowY;
+                        while (SDL_PollEvent(&event)) {
+                            if (event.type == SDL_MOUSEMOTION) {
+                                windowX = initialX + event.motion.x - event.button.x;
+                                windowY = initialY + event.motion.y - event.button.y;
+                                SDL_SetWindowPosition(window, windowX, windowY);
+                            } else if (event.type == SDL_MOUSEBUTTONUP &&
+                                       event.button.button == SDL_BUTTON_LEFT) {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Delay to reduce CPU usage
+        SDL_Delay(10);
+    }
+
+    // Clean up
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
